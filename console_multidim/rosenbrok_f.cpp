11e5fd26138a79e15fe5520b19f74718ca644100
Herikoid <47945489+Herikoid@users.noreply.github.com>
2022-03-21 14:15:21 +0300

    corr

--- console_multidim/rosenbrok_f.cpp
+++ console_multidim/rosenbrok_f.cpp
@@ -13,5 +13,5 @@ std::vector<double> rosenbrok_f::grad(std::vector<double> x)
     if (x.size() != 2) {
         throw std::invalid_argument("argument must be 2d");
     }
-    return std::vector<double> {- 2 * (1 - x[0]) - 400 * x[0] * (x[1] - x[0]), 200 * (x[1] - x[0])};
+    return std::vector<double> { 400 * pow(x[0], 3) - 400 * x[0] * x[1] + 2 * x[0] - 2 , 200 * (x[1] - pow(x[0], 2))};
 }
