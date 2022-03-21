736f33815c99b222df9befdd4aaa3e5aa7e9a26b
Herikoid <47945489+Herikoid@users.noreply.github.com>
2022-03-21 04:58:58 +0300

    upd doxygen

--- doxygen/html/search/classes_5.js
+++ doxygen/html/search/classes_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['random_5fsearch_0',['random_search',['../classrandom__search.html',1,'']]]
+  ['random_5fsearch_0',['random_search',['../classrandom__search.html',1,'']]],
+  ['rosenbrok_5ff_1',['rosenbrok_f',['../classrosenbrok__f.html',1,'']]]
 ];
